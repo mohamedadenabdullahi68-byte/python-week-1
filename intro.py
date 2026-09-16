c8b6858ca0309fdf0609214bed106038b81d2d33
Mohamed Aden Abdullahi <mohamedadenabdullahi68@gmail.com>
2026-09-16 16:17:18 +0300

    Add files via upload

---
+++ intro.py
@@ -0,0 +1,11 @@
+# Introduce myself using variables and formatted output
+
+name = "Mohamed Aden Abdullahi"
+age = 27
+favourite_language = "Python"
+goal = "build my own web applications from scratch"
+
+print(f"Hi, my name is {name}.")
+print(f"I am {age} years old.")
+print(f"My favourite programming language is {favourite_language}.")
+print(f"My goal for this course is to {goal}.")
